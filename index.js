61fc3edd4b0a59d333d83d20b9571d309a784171
JAHID HASSAN <75775860+jahid001-dev@users.noreply.github.com>
2023-10-21 03:39:22 +0600

    environment variables successfully added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 // middleware
@@ -9,12 +10,8 @@ app.use(cors())
 app.use(express.json())
 
 
-//brandProject
-//wPTvTN3i76sCklYc
 
-
-
-const uri = "mongodb+srv://brandProject:wPTvTN3i76sCklYc@cluster0.jthhcqv.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jthhcqv.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
